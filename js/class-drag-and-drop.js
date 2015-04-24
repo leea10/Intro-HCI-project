fcbc645c5365b9eb0067805254d3fe3b1df5b382
Ariel Lee <ariel.lee.1995@gmail.com>
2015-04-24 17:24:48 -0400

    Finish dragging effects, must implement dropping

--- js/class-drag-and-drop.js
+++ js/class-drag-and-drop.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
+
+	var dropped = false;
 	$('.draggable-class').draggable({
+		appendTo: 'body',
 		containment: 'body',
 		cursor: 'move',
-		scroll: false,
-		stack: '.draggable-class',
-		revert: 'invalid',
 		helper: 'clone',
-		appendTo: 'body',
-		opacity: 0.8
+		opacity: 0.8,
+		revert: 'invalid',
+		stack: '.draggable-class',
+
+		// takes care of the "ghost" underneath the draggable class
+		start: function(event, ui) {
+			dropped = false;
+			$(this).addClass("ghost", {
+				duration: 'normal'
+			});
+		},
+		stop: function(event, ui) {
+			if(dropped == false) {
+				$(this).removeClass("ghost", {
+					duration: 'normal'
+				});
+			}
+		}
 	});
 });
