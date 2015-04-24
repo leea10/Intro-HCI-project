17903285129db1e6aaa0dc2dfbcedfc87d8bc20a
Ariel Lee <ariel.lee.1995@gmail.com>
2015-04-24 00:38:19 -0400

    more color switcharounds

--- js/sidebar-categories.js
+++ js/sidebar-categories.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
 		$("#core-req-toggle span").toggleClass("glyphicon-chevron-up");
 		$("#core-req-toggle span").toggleClass("glyphicon-chevron-down");
 	});
+
+	$("#math-req-toggle").click(function(){
+		$("#math-req").slideToggle();
+		$("#math-req-toggle span").toggleClass("glyphicon-chevron-up");
+		$("#math-req-toggle span").toggleClass("glyphicon-chevron-down");
+	});
 });
